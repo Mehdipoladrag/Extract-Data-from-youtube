8e944b38bff0b8d4aafe671c80a80105dd8e8224
Mehdi Poladrag <mehdipoladrag1382@gmail.com>
2025-10-15 18:38:34 +0330

    Final Code For Extrace mp3 and srt from YT

---
+++ downloader.py
@@ -0,0 +1,33 @@
+import yt_dlp # type: ignore
+
+class Download:
+    def __init__(self, url):
+        self.url = url
+        self.ydl_opts = {} 
+
+    def configs(self):
+        self.ydl_opts = {
+            "format": "bestaudio/best", 
+            "outtmpl": "C:/Users/YOO/Desktop/O/%(title)s.%(ext)s",  
+            "ffmpeg_location": "C:/Users/YOO/Desktop/O/ffmpeg.exe",  
+            "cookiefile": "C:/Users/YOO/Desktop/O/www.youtube.com_cookies.txt", # Cookies must related to your youtube video link
+            "writesubtitles": True,          
+            "writeautomaticsub": True,       
+            "subtitleslangs": ["fa"], 
+            "subtitlesformat": "srt",       
+            "postprocessors": [{
+                "key": "FFmpegExtractAudio",
+                "preferredcodec": "mp3",
+                "preferredquality": "192",
+            }],
+        }
+
+    def download(self):
+        if not self.ydl_opts:
+            self.configs()  
+        with yt_dlp.YoutubeDL(self.ydl_opts) as ydl:
+            ydl.download([self.url])
+
+
+video = Download("https://www.youtube.com/watch?v=LtkqV77njsM") # Your Random Url for Extract 
+video.download()
